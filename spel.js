632699e4e1e1cee25c83b7719295fb68950b67ad
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2014-08-30 00:44:39 +0200

    MOD: Maak het spel klaar voor de Dojo

--- spel.js
+++ spel.js
@@ -1,28 +1,10 @@
 Galg.prototype.klaarMetLaden = function(galg){
-  // stap 1) kies een willekeurig woord
-  galg.kiesWillekeurigWoord();
-
-  // stap 2) laat het woord als streepjes zien
-  galg.laatHintsZien();
+  // Vanaf hier is het helemaal aan jou!
+  // Het spel is klaar met laden, wat moet er als eerste gebeuren?
 }
 
 Galg.prototype.opKnopGeklikt = function(galg){
-  
-  var ingevuldeLetter = galg.ingevuldeLetter();
 
-  if(galg.zitLetterInWoord(ingevuldeLetter)){
-    galg.onthulLetter(ingevuldeLetter);
-  }
-  else{
-    galg.fout();
-  }
-
-  if(galg.spelerIsAf()){
-    galg.helaas();
-  }
-
-  if(galg.spelerHeeftGewonnen()){
-    galg.feest();
-  }
+  // Ok, iemand heeft op de knop "controleer geklikt". Hoe gaan we dit afhandelen?
 
 }
